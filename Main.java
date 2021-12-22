dc37f3394388df55c097db8f09969030c591c9db
gabrielfrimodig <gw@pop-os.localdomain>
2021-12-22 18:47:06 +0100

    Example Log4j

---
+++ Main.java
@@ -0,0 +1,13 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
+    public static void main(String[] args){
+        System.setProperty("com.sun.jndi.ldap.object.trustURLCodebase", "true");
+
+        String user = args[0];
+        logger.error("Hello: " + user);
+    }
+}
